fdb3c68cc94500b60fa3a29dbe30f0a01a223457
ingvarr <igor.bilinskyy@gmail.com>
2019-10-03 20:32:23 +0300

    add new file 20191003_01.py

---
+++ 20191003_01.py
@@ -0,0 +1,8 @@
+# pyramid
+heigth = int(input("type heigth > "))
+for i in range(heigth):
+    for y in range(heigth - i):
+        print(" ", end="")
+    for j in range(i + 2):
+        print("#", end="")
+    print()
