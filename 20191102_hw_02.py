8d4570ac878d75eddca300fd3b64f1d38f4c3205
ingvarr <igor.bilinskyy@gmail.com>
2019-11-02 14:02:35 +0200

    Home Work of week3

--- 20191102_hw_02.py
+++ 20191102_hw_02.py
@@ -7,14 +7,16 @@ year_lst = [1990, 2000, 1995, 2002]
 for nm, yr in zip(name_lst, year_lst):
     name_dct[nm] = yr
 
-print(name_dct)
+print("We make dictionary :", name_dct)
 
-print("\nsorted by Name")
+print("\nDictionary sorted by Name")
 for i in sorted(name_dct):
     print(i, ":", name_dct[i])
 
-print("\nsorted by Year")
+print("\nDictionary sorted by Year")
 for vls in sorted(name_dct.values()):
     for ks in name_dct:
         if name_dct[ks] == vls:
             print(ks, ":", name_dct[ks])
+
+
