bd15397f1a7e393d1c487e50e3c3dc68b199c60e
ingvarr <igor.bilinskyy@gmail.com>
2019-11-10 00:16:49 +0200

    Homework '*_hw_file_02'

---
+++ 20191109_hw_file_02.py
@@ -0,0 +1,27 @@
+'''
+Write a function that takes a file name. It reads and outputs statistics:
+- number of rows
+- number of words
+- number of letters
+'''
+
+
+def file_count(name_f):
+    with open(name_f) as file_o:
+        text = file_o.read()
+        f_lines = text.split('\n')
+        print(f'file {name_f} have {len(f_lines)} lines')
+        f_words = []
+        for line in f_lines:
+            f_words += (line.split(' '))
+        print(f'file {name_f} have {len(f_words)} words')
+        f_lett = 0
+        for lett in f_words:
+            f_lett += len(lett)
+        print(f'file {name_f} have {f_lett} letters')
+
+
+    return
+
+
+file_count("text.txt")
